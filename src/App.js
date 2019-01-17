0e1bfd7c38b73a0a026a57f82a5af65760228ae2
samijelliti <samijelliti@yahoo.fr>
2019-01-17 12:02:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import blue from './blue.jpg';
+import mcvisa from './mcvisa.png';
+import puce from './puce.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="maincard">
+        <p className="title1">CREDIT CARD</p>
+        <img className="puce" src={puce}/>
+        <div className="numcard">
+            <p>7865 6543 1234 0987</p>
+        </div>
+        <div className="valid">
+            <div className="valid1">
+                <p>5422</p>
+                <p>Valid <br/>Thru</p>
+
+            </div>
+            <div>
+                <p>MONTH/YEAR<br/>11/05</p>
+
+            </div>
+            <img className="mastercard" src={mcvisa}/>
+        </div>
+        <p className="title2">CARDHOLDER</p>
+
+    </div>
+    
+
+        
       </div>
     );
   }
